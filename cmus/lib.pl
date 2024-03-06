08b59df5ff3e48b58f25784002b54f748e797328
Minnowfeather <matthewparedes2k3@gmail.com>
2024-03-05 16:36:34 -0800

    Init

---
+++ cmus/lib.pl
@@ -0,0 +1,263 @@
+/home/furry/Desktop/Files/Music/Tenma/05. 3L - Miracle∞Hinacle.flac
+/home/furry/Desktop/Files/Music/Tenma/09 3L - タイニーリトル・アジアンタム.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - Uso no Hibana.flac
+/home/furry/Desktop/Files/Music/Tenma/Aiobahn & Yunomi - ゆのもきゅ - 02 枕元にゴースト (feat. nicamoq).flac
+/home/furry/Desktop/Files/Music/Tenma/07. special, but not special.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 - Kisetsu wa Tsugitsugi Shinde Iku.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. 月曜日.mp3
+/home/furry/Desktop/Files/Music/Tenma/amazarashi - Sora ni Utaeba.mp3
+/home/furry/Desktop/Files/Music/Tenma/08-re-re.mp3
+/home/furry/Desktop/Files/Music/Tenma/PON DE SNAIL.mp3
+/home/furry/Desktop/Files/Music/Tenma/シンクロニシティ.mp3
+/home/furry/Desktop/Files/Music/Tenma/Hare Hare Yukai.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. カミイロアワセ.mp3
+/home/furry/Desktop/Files/Music/Tenma/boc - karma.mp3
+/home/furry/Desktop/Files/Music/Tenma/08 FLY HIGH!!.flac
+/home/furry/Desktop/Files/Music/Tenma/01. Good Morning World!.flac
+/home/furry/Desktop/Files/Music/Tenma/06 - dreamless wanderer.mp3
+/home/furry/Desktop/Files/Music/Tenma/07 - NUCLEAR-STAR.mp3
+/home/furry/Desktop/Files/Music/Tenma/06 - Tsukidourou.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 - Colourful.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 - Connect.mp3
+/home/furry/Desktop/Files/Music/Tenma/ClariS_-_Hologram_(mp3.pm).mp3
+/home/furry/Desktop/Files/Music/Tenma/01 - Luminous.mp3
+/home/furry/Desktop/Files/Music/Tenma/01.asphyxia.mp3
+/home/furry/Desktop/Files/Music/Tenma/02.Lamp.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. 絶体絶命.mp3
+/home/furry/Desktop/Files/Music/Tenma/aiyueni_remix.mp3
+/home/furry/Desktop/Files/Music/Tenma/twintail.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. Ghost Rule.flac
+/home/furry/Desktop/Files/Music/Tenma/05. DSCF.flac
+/home/furry/Desktop/Files/Music/Tenma/02. アンドロイドガール.flac
+/home/furry/Desktop/Files/Music/Tenma/11. 愛言葉Ⅲ.flac
+/home/furry/Desktop/Files/Music/Tenma/08. 紅楼 ～ Eastern Dream.mp3
+/home/furry/Desktop/Files/Music/Tenma/03 Extra terrestrial Biological Entities.flac
+/home/furry/Desktop/Files/Music/Tenma/-FVOst.net- Emiri Kato - terminal terminal.mp3
+/home/furry/Desktop/Files/Music/Tenma/04. BRIGHTEST WAY.mp3
+/home/furry/Desktop/Files/Music/Tenma/(02) [舞花] Closed Wings.flac
+/home/furry/Desktop/Files/Music/Tenma/02. Hail Storm.mp3
+/home/furry/Desktop/Files/Music/Tenma/(04) [Renko!] Lies in Reality.flac
+/home/furry/Desktop/Files/Music/Tenma/02. Memories.mp3
+/home/furry/Desktop/Files/Music/Tenma/02 New World.mp3
+/home/furry/Desktop/Files/Music/Tenma/05. Runway Drive.mp3
+/home/furry/Desktop/Files/Music/Tenma/Time and again.flac
+/home/furry/Desktop/Files/Music/Tenma/[LanotaArcaea] Quon - Feryquitous.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 - Now Loading!!!!.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - SAKURA SKIP.flac
+/home/furry/Desktop/Files/Music/Tenma/01. STEP by STEP UP↑↑↑↑.flac
+/home/furry/Desktop/Files/Music/Tenma/[Forunesia] Contrast.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 Aoi Shiori.flac
+/home/furry/Desktop/Files/Music/Tenma/Kiryuu Moeka - To Be Loved.mp3
+/home/furry/Desktop/Files/Music/Tenma/06 - Midnight Riders.mp3
+/home/furry/Desktop/Files/Music/Tenma/【C81】 Halozy - Tonic Triangle - 08. SAKURA：Sound.mp3
+/home/furry/Desktop/Files/Music/Tenma/【東方 Vocal】 置き傘.mp3
+/home/furry/Desktop/Files/Music/Tenma/02 - Recover Decoration.mp3
+/home/furry/Desktop/Files/Music/Tenma/[Forunesia] Orange Mint.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - Nagareboshi _Shooting Star_.mp3
+/home/furry/Desktop/Files/Music/Tenma/[Forunesia]The World's End.mp3
+/home/furry/Desktop/Files/Music/Tenma/emotional.mp3
+/home/furry/Desktop/Files/Music/Tenma/starlights.mp3
+/home/furry/Desktop/Files/Music/Tenma/02. Tsukinami.mp3
+/home/furry/Desktop/Files/Music/Tenma/11. Kikuo — てんしょう しょうてんしょう (Extended mix).flac
+/home/furry/Desktop/Files/Music/Tenma/[Forunesia] GATE -Sore wa Akatsuki no You ni.flac
+/home/furry/Desktop/Files/Music/Tenma/Kiyono Yasuno - Rocket Beat.mp3
+/home/furry/Desktop/Files/Music/Tenma/Kobaryo - Tool-Assisted Speedcore (TQBF Frame Advance RMX).flac
+/home/furry/Desktop/Files/Music/Tenma/01 To see the future.flac
+/home/furry/Desktop/Files/Music/Tenma/Evanescent.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 My Soul, Your Beats!.flac
+/home/furry/Desktop/Files/Music/Tenma/10. Shinigami Record.mp3
+/home/furry/Desktop/Files/Music/Tenma/[Touhou Vocal] LiLAc Records- Sunset Luv (DNB Version).mp3
+/home/furry/Desktop/Files/Music/Tenma/02. Headphone Actor.mp3
+/home/furry/Desktop/Files/Music/Tenma/04. Yuukei Yesterday.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. 紅蓮華.flac
+/home/furry/Desktop/Files/Music/Tenma/04. Yobanashi Deceive.mp3
+/home/furry/Desktop/Files/Music/Tenma/[Forunesia] Kawaranai Sora.mp3
+/home/furry/Desktop/Files/Music/Tenma/MAN WITH A MISSION - My Hero.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. daze (Re Ver.).mp3
+/home/furry/Desktop/Files/Music/Tenma/01. Children Record.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. Flashback.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. インフェルノ.flac
+/home/furry/Desktop/Files/Music/Tenma/Remembrance.flac
+/home/furry/Desktop/Files/Music/Tenma/shadowgraph.flac
+/home/furry/Desktop/Files/Music/Tenma/01. ワンダードライブ.flac
+/home/furry/Desktop/Files/Music/Tenma/01. 無能.flac
+/home/furry/Desktop/Files/Music/Tenma/candyenyenyenland.mp3
+/home/furry/Desktop/Files/Music/Tenma/Sangenshoku 三原色.mp3
+/home/furry/Desktop/Files/Music/Tenma/08. Divinity (feat. Amy Millan).flac
+/home/furry/Desktop/Files/Music/Tenma/12 - Goodbye To A World.mp3
+/home/furry/Desktop/Files/Music/Tenma/02 - Sad Machine.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. Shelter.flac
+/home/furry/Desktop/Files/Music/Tenma/Colorful Minutes.mp3
+/home/furry/Desktop/Files/Music/Tenma/Nandemonaiya (Movie Ver.).mp3
+/home/furry/Desktop/Files/Music/Tenma/[FVOst.net] RADWIMPS - Zen Zen Zense.mp3
+/home/furry/Desktop/Files/Music/Tenma/Radwimps_-_Dream_Lantern_(mp3.pm).mp3
+/home/furry/Desktop/Files/Music/Tenma/01. LIFE.flac
+/home/furry/Desktop/Files/Music/Tenma/[Forunesia] Mikazuki.mp3
+/home/furry/Desktop/Files/Music/Tenma/02. 町かどタンジェント.flac
+/home/furry/Desktop/Files/Music/Tenma/04. (awa).mp3
+/home/furry/Desktop/Files/Music/Tenma/【東方ヴォーカルPV】全力ハッピーライフ【森羅万象公式】.mp3
+/home/furry/Desktop/Files/Music/Tenma/Shiori Izawa - Tabi no Hidarite, Saihate no Migite [Nanachi ver.].mp3
+/home/furry/Desktop/Files/Music/Tenma/01 八月の夜.m4a
+/home/furry/Desktop/Files/Music/Tenma/-FVOst.net- Sora Amamiya, Rie Takahashi, Ai Kayano - Oie ni Kaeritai.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 - New Stranger.flac
+/home/furry/Desktop/Files/Music/Tenma/01 Daisy.flac
+/home/furry/Desktop/Files/Music/Tenma/01. Genesis.flac
+/home/furry/Desktop/Files/Music/Tenma/01 Rashisa.flac
+/home/furry/Desktop/Files/Music/Tenma/01 My Dearest.flac
+/home/furry/Desktop/Files/Music/Tenma/03. Hero.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - Kimi no Shira nai Monogatari.mp3
+/home/furry/Desktop/Files/Music/Tenma/02. Kimi no Shiranai Monogatari.flac
+/home/furry/Desktop/Files/Music/Tenma/07.Recollector (Original Mix).flac
+/home/furry/Desktop/Files/Music/Tenma/02. Book-end, Happy-end.mp3
+/home/furry/Desktop/Files/Music/Tenma/[128K] Tia - Deal with the devil.mp3
+/home/furry/Desktop/Files/Music/Tenma/01.Creep.flac
+/home/furry/Desktop/Files/Music/Tenma/Triple-Q_Nyanderwall.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 azure.flac
+/home/furry/Desktop/Files/Music/Tenma/01. 春が来てぼくら.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 Houkago Di(e)stra(u)ction.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - Harumodoki.flac
+/home/furry/Desktop/Files/Music/Tenma/夢のような.mp3
+/home/furry/Desktop/Files/Music/Tenma/YUC'e - Future Candy - 06 Cinderella Syndrome.mp3
+/home/furry/Desktop/Files/Music/Tenma/YUC'e - Future Candy - 02 Future Candy.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. さよならバイスタンダー.flac
+/home/furry/Desktop/Files/Music/Tenma/Yunomi - ジェリーフィッシュ (feat. ローラーガール) - Single - 01 ジェリーフィッシュ (feat. ローラーガール).flac
+/home/furry/Desktop/Files/Music/Tenma/Yunomi - ミラクルシュガーランド (feat. 桃箱) - 01 ミラクルシュガーランド (feat. 桃箱).flac
+/home/furry/Desktop/Files/Music/Tenma/Yunomi - ゆのもきゅ - 01 めんたいコズミック (feat. nicamoq).flac
+/home/furry/Desktop/Files/Music/Tenma/02 - 惑星ラビット.flac
+/home/furry/Desktop/Files/Music/Tenma/Yunomi - ゆのもきゅ - 08 東京シュノーケル (feat. nicamoq).flac
+/home/furry/Desktop/Files/Music/Tenma/04 - さよならインベーダー.flac
+/home/furry/Desktop/Files/Music/Tenma/09 - 大江戸コントローラー (Batsu Remix).flac
+/home/furry/Desktop/Files/Music/Tenma/01 - 大江戸コントローラー.flac
+/home/furry/Desktop/Files/Music/Tenma/[OSTnime] YURiKA Kyoumen no Nami.mp3
+/home/furry/Desktop/Files/Music/Tenma/02.L2U.flac
+/home/furry/Desktop/Files/Music/Tenma/02. Amadeus.flac
+/home/furry/Desktop/Files/Music/Tenma/2-0003369699.flac
+/home/furry/Desktop/Files/Music/Tenma/01. ガヴリールドロップキック.flac
+/home/furry/Desktop/Files/Music/Tenma/01. ハレルヤ☆エッサイム.flac
+/home/furry/Desktop/Files/Music/Tenma/06. かめりあ - {albus} (_Silver_ Long ver.).flac
+/home/furry/Desktop/Files/Music/Tenma/01. Alone intelligence.mp3
+/home/furry/Desktop/Files/Music/Tenma/02. Arcology On Permafrost.mp3
+/home/furry/Desktop/Files/Music/Tenma/Artificial Snow.flac
+/home/furry/Desktop/Files/Music/Tenma/Camellia_Autofluorescence_ShortVer.mp3
+/home/furry/Desktop/Files/Music/Tenma/04. Break The Silence.flac
+/home/furry/Desktop/Files/Music/Tenma/12. かめりあ - Chirality (_Dissymmetric_ Long ver.).flac
+/home/furry/Desktop/Files/Music/Tenma/crystallized.flac
+/home/furry/Desktop/Files/Music/Tenma/02. かめりあ - Dyscontrolled Galaxy (_Apoptosis_ Long ver.).flac
+/home/furry/Desktop/Files/Music/Tenma/14. Embracing intelligences.mp3
+/home/furry/Desktop/Files/Music/Tenma/15. かめりあ - Enantiomorphs.flac
+/home/furry/Desktop/Files/Music/Tenma/07. かめりあ - epimerization.flac
+/home/furry/Desktop/Files/Music/Tenma/Exit This Earth's Atomosphere.flac
+/home/furry/Desktop/Files/Music/Tenma/First Town Of This Journey.flac
+/home/furry/Desktop/Files/Music/Tenma/flying in the flow of deep-sea.flac
+/home/furry/Desktop/Files/Music/Tenma/13. Heart of Android _ Even If It's Only By Mechanism.mp3
+/home/furry/Desktop/Files/Music/Tenma/13. かめりあ - kodama.flac
+/home/furry/Desktop/Files/Music/Tenma/liquated.flac
+/home/furry/Desktop/Files/Music/Tenma/Camellia - Literal Crystal In The Cyphisonia (CyphisoniaEP SecretTrack2).mp3
+/home/furry/Desktop/Files/Music/Tenma/10. New Era.mp3
+/home/furry/Desktop/Files/Music/Tenma/04. かめりあ - overcomplexification (_Quaternion_ Long ver.).flac
+/home/furry/Desktop/Files/Music/Tenma/PLANET--SHAPER.flac
+/home/furry/Desktop/Files/Music/Tenma/15. Bonus Track _ Quaoar (For _Thanks Follower 50k_).mp3
+/home/furry/Desktop/Files/Music/Tenma/Rain of Amethyst.flac
+/home/furry/Desktop/Files/Music/Tenma/07. S.A.T.E.L.L.I.T.E..mp3
+/home/furry/Desktop/Files/Music/Tenma/08. This Future (we didn't expect).mp3
+/home/furry/Desktop/Files/Music/Tenma/06. Together forever, my lovely lovely video game cartridges.mp3
+/home/furry/Desktop/Files/Music/Tenma/11. Tojita Sekai.mp3
+/home/furry/Desktop/Files/Music/Tenma/yesterday.flac
+/home/furry/Desktop/Files/Music/Tenma/δ-for the DELTA.flac
+/home/furry/Desktop/Files/Music/Tenma/04. Σtealth-Δash.mp3
+/home/furry/Desktop/Files/Music/Tenma/Hatsune Miku - The weight of a heart (Dubstep) By Camellia.mp3
+/home/furry/Desktop/Files/Music/Tenma/09 - Looking for a New Adventure (feat. Nanahira).mp3
+/home/furry/Desktop/Files/Music/Tenma/05 - Touhiron (feat. Nanahira).mp3
+/home/furry/Desktop/Files/Music/Tenma/01. カラーズぱわーにおまかせろ！.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. よいまちカンターレ.flac
+/home/furry/Desktop/Files/Music/Tenma/-FVOst.net- Sayuri - Sore wa Chiisana Hikari no you na.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. 平行線.flac
+/home/furry/Desktop/Files/Music/Tenma/05. アディショナルメモリー.mp3
+/home/furry/Desktop/Files/Music/Tenma/02. イマジナリーリロード.mp3
+/home/furry/Desktop/Files/Music/Tenma/07. リマインドブルー.mp3
+/home/furry/Desktop/Files/Music/Tenma/03. 失想ワアド.mp3
+/home/furry/Desktop/Files/Music/Tenma/Soraru_Kimi_no_me_wo_(mp3co.biz).mp3
+/home/furry/Desktop/Files/Music/Tenma/01. 花ハ踊レヤいろはにほ.flac
+/home/furry/Desktop/Files/Music/Tenma/01 More One Night.flac
+/home/furry/Desktop/Files/Music/Tenma/01 Ugoku, Ugoku.flac
+/home/furry/Desktop/Files/Music/Tenma/06 - Petals.mp3
+/home/furry/Desktop/Files/Music/Tenma/08 - Raririreru,Ririro,Riraro.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 終末のミュートロギア.flac
+/home/furry/Desktop/Files/Music/Tenma/05. Konoha no Sekai Jijou.mp3
+/home/furry/Desktop/Files/Music/Tenma/TouhouDrumnBass #38 Pilgrim -LNB6 Remix- [HD].mp3
+/home/furry/Desktop/Files/Music/Tenma/「東方」 Pilgrim 「はちみつれもん × Aftergrow」.mp3
+/home/furry/Desktop/Files/Music/Tenma/06 アップルドットコム.flac
+/home/furry/Desktop/Files/Music/Tenma/03 おばけのウケねらい.flac
+/home/furry/Desktop/Files/Music/Tenma/01 Arifureta Sekai Seifuku.flac
+/home/furry/Desktop/Files/Music/Tenma/12. きみも悪い人でよかった.m4a
+/home/furry/Desktop/Files/Music/Tenma/11. すきなことだけでいいです.m4a
+/home/furry/Desktop/Files/Music/Tenma/Slow Motion.mp3
+/home/furry/Desktop/Files/Music/Tenma/11 ぜろ.flac
+/home/furry/Desktop/Files/Music/Tenma/03 モチベーションが死んでる.m4a
+/home/furry/Desktop/Files/Music/Tenma/10 内臓ありますか.flac
+/home/furry/Desktop/Files/Music/Tenma/12 君が生きてなくてよかった.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - OWARASE NIGHT.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - オドループ.flac
+/home/furry/Desktop/Files/Music/Tenma/01 Bon Appétit♡S.flac
+/home/furry/Desktop/Files/Music/Tenma/02. 朝焼けと熱帯魚.mp3
+/home/furry/Desktop/Files/Music/Tenma/ミカヅキBIGWAVE - Tsundere Disco ツンデレディスコ.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 - Pallet.mp3
+/home/furry/Desktop/Files/Music/Tenma/04 - endroll.mp3
+/home/furry/Desktop/Files/Music/Tenma/06 - Boku no Friend.flac
+/home/furry/Desktop/Files/Music/Tenma/02.唄片の人魚.mp3
+/home/furry/Desktop/Files/Music/Tenma/青の都.mp3
+/home/furry/Desktop/Files/Music/Tenma/09.curse upon me!.mp3
+/home/furry/Desktop/Files/Music/Tenma/03. Neko Meshi.flac
+/home/furry/Desktop/Files/Music/Tenma/04. パステルレイン.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. 三月がずっと続けばいい.mp3
+/home/furry/Desktop/Files/Music/Tenma/10. 街路、ライトの灯りだけ.mp3
+/home/furry/Desktop/Files/Music/Tenma/05. 青春なんていらないわ.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. Waikyou Shen Shoujing.flac
+/home/furry/Desktop/Files/Music/Tenma/14. happy bite.flac
+/home/furry/Desktop/Files/Music/Tenma/Majo ni Naritai Hime to Hime ni Naritai Majo.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 - Dramatic Market Ride.flac
+/home/furry/Desktop/Files/Music/Tenma/01 Neguse.flac
+/home/furry/Desktop/Files/Music/Tenma/01._Neguse.mp3
+/home/furry/Desktop/Files/Music/Tenma/18. もうそう♡えくすぷれす.flac
+/home/furry/Desktop/Files/Music/Tenma/04. 恋愛サーキュレーション.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - Aimai Net Darling.flac
+/home/furry/Desktop/Files/Music/Tenma/01.hectopascal.mp3
+/home/furry/Desktop/Files/Music/Tenma/01 Super Driver.flac
+/home/furry/Desktop/Files/Music/Tenma/01. Boken Desho Desho.flac
+/home/furry/Desktop/Files/Music/Tenma/1 - Hare Hare Yukai.flac
+/home/furry/Desktop/Files/Music/Tenma/01 ALL LOVES BLAZING.flac
+/home/furry/Desktop/Files/Music/Tenma/01. Gekisou Gungnir.flac
+/home/furry/Desktop/Files/Music/Tenma/01. Seigi wo Shinjite, Nigirishimete.flac
+/home/furry/Desktop/Files/Music/Tenma/02. Watashi to Iu Oto Hibiki Sono Saki ni.flac
+/home/furry/Desktop/Files/Music/Tenma/01. staple stable.flac
+/home/furry/Desktop/Files/Music/Tenma/07. 二言目.flac
+/home/furry/Desktop/Files/Music/Tenma/02. Stand up! Lady!!.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - 白銀の炎 -keep the faith-.flac
+/home/furry/Desktop/Files/Music/Tenma/02 Sci-Fi ROMANCE TRAVELER.mp3
+/home/furry/Desktop/Files/Music/Tenma/02. Edge Works of Goddess ZABABA.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - Ai wo Torimadose!!​.flac
+/home/furry/Desktop/Files/Music/Tenma/02 Tsuki ga Kirei.flac
+/home/furry/Desktop/Files/Music/Tenma/09. Lost Time Memory.mp3
+/home/furry/Desktop/Files/Music/Tenma/02 - 100% nai nai nai.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - Nippon Egao Hyakkei.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - Oato ga Yoroshikute... Yo!.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - Defender'Z Brand！.flac
+/home/furry/Desktop/Files/Music/Tenma/01. TESTAMENT.flac
+/home/furry/Desktop/Files/Music/Tenma/01. Zettou Ame no Habakiri.flac
+/home/furry/Desktop/Files/Music/Tenma/01. Senkin Daurdabla.flac
+/home/furry/Desktop/Files/Music/Tenma/06. Kagerou Days.mp3
+/home/furry/Desktop/Files/Music/Tenma/06. Toumei Answer.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. Genkai Toppa G-beat.flac
+/home/furry/Desktop/Files/Music/Tenma/002-main actor.flac
+/home/furry/Desktop/Files/Music/Tenma/001-カワキヲアメク.flac
+/home/furry/Desktop/Files/Music/Tenma/004-ホロネス.flac
+/home/furry/Desktop/Files/Music/Tenma/003-ライラック.flac
+/home/furry/Desktop/Files/Music/Tenma/11. perfect slumbers.flac
+/home/furry/Desktop/Files/Music/Tenma/01 - Minna de 5ji Pittan.flac
+/home/furry/Desktop/Files/Music/Tenma/01. Gokuren Igalima.flac
+/home/furry/Desktop/Files/Music/Tenma/09. 白金ディスコ.flac
+/home/furry/Desktop/Files/Music/Tenma/08. marshmallow justice.flac
+/home/furry/Desktop/Files/Music/Tenma/01. veil.mp3
+/home/furry/Desktop/Files/Music/Tenma/01. Futurism.flac
+/home/furry/Desktop/Files/Music/Tenma/01. GUN BULLET XXX.flac
+/home/furry/Desktop/Files/Music/Tenma/01. Next Destination.flac
---
+++ neovim/init.vim
@@ -0,0 +1,146 @@
+packadd mason.nvim
+packadd mason-lspconfig.nvim
+packadd nvim-lspconfig
+
+packadd nvim-cmp
+packadd cmp-nvim-lsp
+
+packadd nerdtree
+packadd lualine.nvim
+packadd nvim-colorizer.lua
+packadd ayu-vim
+
+packadd nvim-treesitter
+packadd nvim-treesitter-context
+
+set number
+set number
+syntax on
+set t_Co=256
+set mouse=a
+set termguicolors
+let ayucolor="mirage"
+colorscheme ayu
+
+lua << END
+require('mason').setup()
+require('mason-lspconfig').setup()
+
+-- cmp nvim lsp setup
+require'cmp'.setup {
+  sources = {
+    { name = 'nvim_lsp' }
+  }
+}
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'ayu_mirage',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
+-- Attaches to every FileType mode
+require 'colorizer'.setup()
+
+require'treesitter-context'.setup{
+  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  line_numbers = true,
+  multiline_threshold = 20, -- Maximum number of lines to show for a single context
+  trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = nil,
+  zindex = 20, -- The Z-index of the context window
+  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+}
+
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup{capabilities = capabilities}
+lspconfig.angularls.setup{capabilities = capabilities}
+lspconfig.jdtls.setup{capabilities = capabilities}
+lspconfig.html.setup{capabilities = capabilities}
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+END
+
+"let g:airline_theme='jellybeans'
+"let g:airline_powerline_fonts = 1
+
+
